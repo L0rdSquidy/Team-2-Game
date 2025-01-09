44695251564afc5dde7824b12fe02c8b0e84867a
PerssonPerson <elliot.rg.persson@gmail.com>
2025-01-09 13:08:48 +0100

    mp wip

---
+++ Team2Game/Assets/Multiplayer stuff/Scripts/Resource.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    private int[] resources = new int[5];
+    //resources[0] for wood, [1] for wheat, [2] for iron, [3] for bread, [4] for weapons
+
+
+    
+
+
+}
