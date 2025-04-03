6899bebe78257af9af95f8ebd3f74e6e7ed824bc
L0rdSquidy <144682503+L0rdSquidy@users.noreply.github.com>
2025-04-03 10:31:52 +0200

    boom

--- Team2Game/Assets/Scripts/Switch.cs
+++ Team2Game/Assets/Scripts/Switch.cs
@@ -5,12 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
-    public void sweden()
-    {
-        SceneManager.LoadScene(7);
-    }
-    public void nl()
-    {
-        SceneManager.LoadScene(7);
-    }
+	public void sweden()
+	{
+		SceneManager.LoadScene(7);
+	}
+	public void nl()
+	{
+		SceneManager.LoadScene(7);
+	}
+	
+	public void BacktoScene(bool ISnl)
+	
+	{
+		if (ISnl)
+		{
+			SceneManager.LoadScene(2);
+		} else
+		{
+			SceneManager.LoadScene(1);
+		}
+	}
 }
